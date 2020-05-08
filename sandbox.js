8b77bb738954a1e990a1c29dbc6c94363ad96b85
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-05-08 12:19:10 -0400

    Finished type checking variables

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var character = 'luigi';
+var age = 30;
+var isBlackBelt = false;
+character = 'mario';
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7));
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+let character = 'luigi';
+let age = 30;
+let isBlackBelt = false;
+
+character = 'mario';
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(7));
