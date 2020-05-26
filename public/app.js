fc782ca3beccea80dc07bdce2a79f20edce5c105
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-05-26 12:04:34 -0400

    Finished project

--- public/app.js
+++ public/app.js
@@ -1,5 +1,24 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 import { Payment } from './models/Payment.js';
 import { Invoice } from './models/invoice.js';
+import { ListTemplate } from './models/ListTemplate.js';
 // let docOne: HasFormatter;
 // let docTwo: HasFormatter;
 // docOne = new Invoice('yoshi', 'web work', 250);
@@ -24,15 +43,65 @@ var type = document.getElementById('type');
 var tofrom = document.getElementById('tofrom');
 var details = document.getElementById('details');
 var amount = document.getElementById('amount');
+// list template instance
+var ul = document.querySelector('ul');
+var list = new ListTemplate(ul);
 newFormItem.addEventListener('submit', function (e) {
     // prevent the page refresh when the form is submitted
     e.preventDefault();
+    var values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     var doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new (Invoice.bind.apply(Invoice, __spreadArrays([void 0], values)))();
     }
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
+// GENERICS
+var addUID = function (obj) {
+    var uid = Math.floor(Math.random() * 100);
+    return __assign(__assign({}, obj), { uid: uid });
+};
+var docOne = addUID({ name: 'yoshi', age: 40 });
+console.log(docOne);
+var docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'tony' }
+};
+var docFour = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk']
+};
+console.log(docThree, docFour);
+// ENUMS
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+})(ResourceType || (ResourceType = {}));
+var docFive = {
+    uid: 1,
+    resourceType: ResourceType.BOOK,
+    data: { title: 'name of the wind' }
+};
+var docSix = {
+    uid: 10,
+    resourceType: ResourceType.DIRECTOR,
+    data: { name: 'yoshi' }
+};
+console.log(docFive, docSix);
+// TUPLES
+var arr = ['yoshi', 25, true];
+var tup = ['yohsi', 25, true];
+tup[0] = 'mario';
+tup[1] = 50;
+tup[2] = false;
+var student;
+student = ['ryu', 22555];
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Payment } from './models/Payment.js';
 import { Invoice } from './models/invoice.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
+import { ListTemplate } from './models/ListTemplate.js';
 
 // let docOne: HasFormatter;
 // let docTwo: HasFormatter;
@@ -36,17 +37,96 @@ const tofrom = document.getElementById('tofrom') as HTMLInputElement;
 const details = document.getElementById('details') as HTMLInputElement;
 const amount = document.getElementById('amount') as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
 newFormItem.addEventListener('submit', (e: Event) => {
   // prevent the page refresh when the form is submitted
   e.preventDefault();
 
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
+
   let doc: HasFormatter;
 
   if (type.value === 'invoice') {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Invoice(...values);
   } else {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  list.render(doc, type.value, 'end');
 });
+
+// GENERICS
+const addUID = <T extends object>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+let docOne = addUID({ name: 'yoshi', age: 40 });
+
+console.log(docOne);
+
+// with interfaces
+interface Resource<T> {
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+const docThree: Resource<object> = {
+  uid: 1,
+  resourceName: 'person',
+  data: { name: 'tony' }
+};
+
+const docFour: Resource<string[]> = {
+  uid: 2,
+  resourceName: 'shoppingList',
+  data: ['bread', 'milk']
+};
+
+console.log(docThree, docFour);
+
+// ENUMS
+
+enum ResourceType {
+  BOOK,
+  AUTHOR,
+  FILM,
+  DIRECTOR
+}
+
+interface ResourceEnum<T> {
+  uid: number;
+  resourceType: ResourceType;
+  data: T;
+}
+const docFive: ResourceEnum<object> = {
+  uid: 1,
+  resourceType: ResourceType.BOOK,
+  data: { title: 'name of the wind' }
+};
+
+const docSix: ResourceEnum<object> = {
+  uid: 10,
+  resourceType: ResourceType.DIRECTOR,
+  data: { name: 'yoshi' }
+};
+
+console.log(docFive, docSix);
+
+// TUPLES
+let arr = ['yoshi', 25, true];
+
+let tup: [string, number, boolean] = ['yohsi', 25, true];
+
+tup[0] = 'mario';
+tup[1] = 50;
+tup[2] = false;
+
+let student: [string, number];
+
+student = ['ryu', 22555];
